6b82a1a73fc82bd9330975ba20dbc85d8739ecfc
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2025-03-05 17:29:33 +0530

    [UPDATE] Storage Function

---
+++ Services/BlobStorageServices.cs
@@ -0,0 +1,68 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLIMB_BE.Services
+{
+    public class BlobStorageService
+    {
+        private readonly string _connectionString;
+        private readonly string _problemsContainerName;
+        private readonly string _resumesContainerName;
+
+        // Constructor that accepts the credentials
+        public BlobStorageService(string connectionString, string problemsContainerName, string resumesContainerName)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            _problemsContainerName = problemsContainerName ?? throw new ArgumentNullException(nameof(problemsContainerName));
+            _resumesContainerName = resumesContainerName ?? throw new ArgumentNullException(nameof(resumesContainerName));
+        }
+
+        // Upload company problems to Blob Storage
+        public async Task UploadCompanyProblemsAsync(string companyName, List<string> problems)
+        {
+            var blobServiceClient = new BlobServiceClient(_connectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(_problemsContainerName);
+            var blobClient = blobContainerClient.GetBlobClient($"{companyName}_problems.txt");
+
+            var content = string.Join("\n", problems);
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                await blobClient.UploadAsync(stream, overwrite: true);
+            }
+        }
+
+        // Retrieve company problems from Blob Storage
+        public async Task<List<string>> GetCompanyProblemsAsync(string companyName)
+        {
+            var blobServiceClient = new BlobServiceClient(_connectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(_problemsContainerName);
+            var blobClient = blobContainerClient.GetBlobClient($"{companyName}_problems.txt");
+
+            var response = await blobClient.DownloadAsync();
+            using (var reader = new StreamReader(response.Value.Content))
+            {
+                var content = await reader.ReadToEndAsync();
+                return content.Split('\n').ToList();
+            }
+        }
+
+        // Upload resume to Blob Storage
+        public async Task<string> UploadResumeAsync(IFormFile resumeFile)
+        {
+            var blobServiceClient = new BlobServiceClient(_connectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(_resumesContainerName);
+            var blobClient = blobContainerClient.GetBlobClient(resumeFile.FileName);
+
+            using (var stream = resumeFile.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, overwrite: true);
+            }
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
